fc3eca3d6092c72169210548b003001eea5dd74c
srinu <srinu11112000@gmail.com>
2023-11-04 10:20:35 +0530

    connecting the k8s

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,8 @@ WORKDIR /app
 COPY package.json .
 RUN npm install
 COPY . .
+# RUN npm run build
 
-CMD [ "npm" "start"]
+EXPOSE 4001
+
+CMD ["npm", "run", "dev"]
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
 
 app.listen(4001, () => {
-    console.log("app is running in the port 4001");
-})
+  console.log("app is running in the port 4001");
+  console.log("test the log!!!!!!!!!!!");
+});
